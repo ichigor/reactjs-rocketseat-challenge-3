0db9664884f81a97dc1768d29740b00222cb54b3
ichigor <igor.moiano@hotmail.com>
2019-04-07 15:16:22 -0300

    Challenge 3 complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import dotenv from 'dotenv';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Routes from './routes';
+
+import './config/ReactotronConfig';
+import 'font-awesome/css/font-awesome.css';
+import './styles.css';
+
+dotenv.config();
+
+const App = () => (
+  <Provider store={store}>
+    <Fragment>
+      <Routes />
+      <ToastContainer autoClose={5000} />
+    </Fragment>
+  </Provider>
+);
 
 export default App;
